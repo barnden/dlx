1bb0bff8e55811c1027832bcc46d32e6fda3c173
barnden <brandon@nguyen.vc>
2026-07-20 18:07:05 -0500

    refactor: fix indentation in Knuth example

--- examples/knuth.cpp
+++ examples/knuth.cpp
@@ -8,14 +8,14 @@ Example (3) in Dancing Links [Knuth00].
 auto main() -> int
 {
     // clang-format off
-        auto entries = std::initializer_list<std::pair<size_t, size_t>> {
-                                { 1, 3 },           { 1, 5 }, { 1, 6 },
-            { 2, 1 },                     { 2, 4 },                     { 2, 7 },
-                    { 3, 2 }, { 3, 3 },                     { 3, 6 },
-            { 4, 1 },                     { 4, 4 },
-                    { 5, 2 },                                         { 5, 7 },
-                                        { 6, 4 }, { 6, 5 },           { 6, 7 }
-        };
+    auto entries = std::initializer_list<std::pair<size_t, size_t>> {
+                            { 1, 3 },           { 1, 5 }, { 1, 6 },
+        { 2, 1 },                     { 2, 4 },                     { 2, 7 },
+                { 3, 2 }, { 3, 3 },                     { 3, 6 },
+        { 4, 1 },                     { 4, 4 },
+                { 5, 2 },                                         { 5, 7 },
+                                    { 6, 4 }, { 6, 5 },           { 6, 7 }
+    };
     // clang-format on
 
     auto solver = DLX::Solver(6, 7, entries);
